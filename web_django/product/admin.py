8e307aac8e8279460b036b5e2e6fa23dc0efe089
GDRAGON-CN <nguyentruonggiang2005vp@gmail.com>
2025-10-15 21:32:53 +0700

    Cập nhật chức năng mới

--- web_django/product/admin.py
+++ web_django/product/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category,Order,OrderItem
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     exclude = ('discount_percent',)  # ẩn trường tính tự động này khỏi form admin
 
 admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
 
